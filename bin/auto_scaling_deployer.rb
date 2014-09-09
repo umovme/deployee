8862cbcd467dd04660741cedfd52c31070aeba2c
robsonbittencourt <robson.luizv@gmail.com>
2014-09-09 17:53:50 -0300

    Alterando a forma como é obtido o load balancer

--- bin/auto_scaling_deployer.rb
+++ bin/auto_scaling_deployer.rb
@@ -2,9 +2,9 @@ require 'aws-sdk'
 
 class AutoScalingDeployer
 
-	def initialize load_balancer, auto_scaling_group
-		@load_balancer = AWS::ELB::LoadBalancer.new load_balancer
-    @auto_scaling_group = AWS::AutoScaling::Group.new auto_scaling_group
+	def initialize auto_scaling_group
+		@auto_scaling_group = AWS::AutoScaling::Group.new auto_scaling_group
+    @load_balancer = @auto_scaling_group.load_balancers[0]
 	end	
 
 	def instance_exists_in_load_balancer? instance_id
@@ -23,14 +23,13 @@ class AutoScalingDeployer
 
       puts "Instances in lb: #{lb_instances_quantity}"
       puts "Desired instances in lb: #{desired_capacity}"
-
+      puts 'Waiting for instances quantity ok in lb. This may take some time...'   
+        
       if lb_instances_quantity == desired_capacity
         puts 'Instances quantity is ok on lb'
         return
       else
         count += 1
-        puts 'Waiting for instances quantity ok in lb'   
-        puts "Times: #{count}"
         sleep(15)
       end  
     end
