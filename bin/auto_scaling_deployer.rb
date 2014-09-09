054071efbbb870b408c6d42324cfc4609fce54ef
robsonbittencourt <robson.luizv@gmail.com>
2014-09-09 16:41:14 -0300

    Criando método para recuperar o desired instances quantity do autoscaling group

--- bin/auto_scaling_deployer.rb
+++ bin/auto_scaling_deployer.rb
@@ -2,8 +2,9 @@ require 'aws-sdk'
 
 class AutoScalingDeployer
 
-	def initialize load_balancer
+	def initialize load_balancer, auto_scaling_group
 		@load_balancer = AWS::ELB::LoadBalancer.new load_balancer
+    @auto_scaling_group = AWS::AutoScaling::Group.new auto_scaling_group
 	end	
 
 	def instance_exists_in_load_balancer? instance_id
@@ -11,5 +12,9 @@ class AutoScalingDeployer
 			return instance_id == instance.id ? true : false
 		end	 
 	end	
+
+  def desired_capacity
+    @auto_scaling_group.desired_capacity
+  end 
 
 end
