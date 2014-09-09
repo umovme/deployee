e416658f5147e6d6fee3e95b0f7874f0a63a2322
robsonbittencourt <robson.luizv@gmail.com>
2014-09-09 17:22:33 -0300

    Criando método para validar se a quantidade de instâncias no lb é igual a quantidade de instâncias desejadas no grupo de as

--- bin/auto_scaling_deployer.rb
+++ bin/auto_scaling_deployer.rb
@@ -13,8 +13,30 @@ class AutoScalingDeployer
 		end	 
 	end	
 
-  def desired_capacity
-    @auto_scaling_group.desired_capacity
-  end 
+  def validate_lb_instances_quantity 
+    puts 'Waiting new instance preparation...'
+    count = 0
+    
+    while count < 60 do 
+      lb_instances_quantity = @load_balancer.instances.count
+      desired_capacity = @auto_scaling_group.desired_capacity
+
+      puts "Instances in lb: #{lb_instances_quantity}"
+      puts "Desired instances in lb: #{desired_capacity}"
+
+      if lb_instances_quantity == desired_capacity
+        puts 'Instances quantity is ok on lb'
+        return
+      else
+        count += 1
+        puts 'Waiting for instances quantity ok in lb'   
+        puts "Times: #{count}"
+        sleep(15)
+      end  
+    end
+
+    puts 'The instances quantity this is not correct. Contact support.'
+    exit 1
+  end  
 
 end
