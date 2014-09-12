cb093eabb49c738edd28470fd5a0f11721b2e4f9
robsonbittencourt <robson.luizv@gmail.com>
2014-09-12 01:20:26 -0300

    Corrigido método que recupera a quantidade de instâncias ok no load balancer

--- bin/auto_scaling_deployer.rb
+++ bin/auto_scaling_deployer.rb
@@ -2,25 +2,25 @@ require 'aws-sdk'
 
 class AutoScalingDeployer
 
-	def initialize auto_scaling_group
-		@auto_scaling_group = AWS::AutoScaling::Group.new auto_scaling_group
+  def initialize auto_scaling_group
+    @auto_scaling_group = AWS::AutoScaling::Group.new auto_scaling_group
     @load_balancer = @auto_scaling_group.load_balancers[0]
-	end	
+  end	
 
-	def instance_exists_in_load_balancer? instance_id
-		@load_balancer.instances.each do |instance|
-			return instance_id == instance.id ? true : false
-		end	 
-	end	
-
-  def get_ok_intances_in_load_balancer
-    instances_ok = 0
+  def instance_exists_in_load_balancer? instance_id
     @load_balancer.instances.each do |instance|
-      if instance.status.to_s == 'running'
-        instances_ok += 1
+      return instance_id == instance.id ? true : false
+    end	 
+  end	
+
+  def quantity_ok_intances_in_load_balancer
+    ok_instances = 0
+    @load_balancer.instances.health.each do |health|
+      if health[:state] == 'InService'
+        ok_instances += 1
       end  
     end 
-    return instances_ok
+    return ok_instances
   end  
 
   def validate_lb_instances_quantity 
@@ -28,7 +28,7 @@ class AutoScalingDeployer
     count = 0
     
     while count < 60 do 
-      lb_instances_quantity = get_ok_intances_in_load_balancer
+      lb_instances_quantity = quantity_ok_intances_in_load_balancer
       desired_capacity = @auto_scaling_group.desired_capacity
   
       if lb_instances_quantity == desired_capacity
