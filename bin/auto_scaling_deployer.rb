adca9593fecc0f1edd63279db76f3d3768187e4d
robsonbittencourt <robson.luizv@gmail.com>
2014-09-22 18:01:27 -0300

    Refatorando o script de deploy

--- bin/auto_scaling_deployer.rb
+++ bin/auto_scaling_deployer.rb
@@ -2,82 +2,60 @@ require 'aws-sdk'
 
 class AutoScalingDeployer
 
-  def initialize auto_scaling_group
-    @auto_scaling_group = AWS::AutoScaling::Group.new auto_scaling_group
-    @load_balancer = @auto_scaling_group.load_balancers[0]
-  end	
+  def initialize group
+    @group = AWS::AutoScaling::Group.new group
+    @load_balancer = @group.load_balancers[0]
+  end
 
-  def deploy_with_one_instance_on_lb
-    instance = @auto_scaling_group.auto_scaling_instances[0]
-    
-    puts 'Increasing auto scaling group'
-    @auto_scaling_group.set_desired_capacity(2)
-    validate_lb_instances_quantity
-    
-    puts 'Terminating old instance'
-    status = instance.terminate(false)
-    puts status.description    
-  end  
-
-  def deploy_with_many_instances_on_lb
-    instances = @auto_scaling_group.auto_scaling_instances
-    
-    instances.each do |instance|
-      puts instance.id
-      if instance_exists_in_load_balancer? instance
-        status = instance.terminate(false)
-        puts status.description 
-        validate_lb_instances_quantity
-      end  
+  def do_deploy
+    @group.auto_scaling_instances.each do |instance|
+      renew(instance) if exists_in_load_balancer?(instance)
     end
+  end
+
+  def exists_in_load_balancer? instance
+    @load_balancer.instances.any? do |lb_instance|
+      instance.id == lb_instance.id
+    end
+  end 
+
+  def renew instance
+    should_increase_max_size = @group.desired_capacity == @group.max_size
+    increase_max_size if should_increase_max_size
       
-  end  
+    puts 'Increasing auto scaling group'
+    increase_desired_capacity
+    wait_until_instances_ok
+    puts instance.terminate(true).description 
 
-  def instance_exists_in_load_balancer? instance
-    puts instance.id
-    @load_balancer.instances.each do |lb_instance|
-      puts lb_instance.id
-      return instance.id == lb_instance.id ? true : false
-    end	 
-  end	
+    decrease_max_size if should_increase_max_size
+  end
 
-  def quantity_ok_intances_in_load_balancer
-    ok_instances = 0
-    @load_balancer.instances.health.each do |health|
-      if health[:state] == 'InService'
-        ok_instances += 1
-      end  
-    end
-    return ok_instances
-  end  
-
-  def validate_lb_instances_quantity 
+  def wait_until_instances_ok 
     puts 'Waiting new instance preparation. This may take some time...'
     sleep(30)
-    count = 0
-    lb_status = false
+    sleep(15) until count_lb_instances == @group.desired_capacity
+    puts 'Instance ok. All right.'
+  end 
 
-    while count < 90 do 
-      lb_instances_quantity = quantity_ok_intances_in_load_balancer
-      desired_capacity = @auto_scaling_group.desired_capacity
-      puts "Desired: #{desired_capacity}"
-      if lb_instances_quantity == desired_capacity
-        puts 'Instance ok. All right.'
-        lb_status = true
-        break
-      else
-        count += 1
-        sleep(15)
-      end  
+  def count_lb_instances
+    @load_balancer.instances.health.count do |health|
+      health[:state] == 'InService'
     end
+  end
 
-    if !lb_status
-      puts 'Instance not initialized. Contact support.'
-      exit 1
-    end
-      
-  end  
+  def increase_desired_capacity
+    @group.set_desired_capacity @group.desired_capacity + 1
+  end
 
+  def increase_max_size
+    options = {:max_size => @group.max_size + 1}
+    @group.update options
+  end
+
+  def decrease_max_size
+    options = {:max_size => @group.max_size - 1}
+    @group.update options
+  end
+  
 end
-
-AutoScalingDeployer.new('test-for-deployer').deploy_with_many_instances_on_lb
